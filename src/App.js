40373bb4bc83ddc1bf9ee5c38c693db9965e8106
Davide Vernizzi <davide@vernizzis.it>
2020-03-28 22:56:35 +0100

    feat(): initial import feat(): prettier

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import ShopList from "./containers/ShopList";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ShopList />
+      </header>
+    </div>
+  );
+}
+
+export default App;
