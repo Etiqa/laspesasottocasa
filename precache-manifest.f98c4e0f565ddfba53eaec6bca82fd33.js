3d5b1fee6808b5d519f5f445b35583626d10d7f2
Andrea Giardina <andrea@etiqa.it>
2020-03-30 11:19:05 +0200

    Updates

--- precache-manifest.f98c4e0f565ddfba53eaec6bca82fd33.js
+++ precache-manifest.f98c4e0f565ddfba53eaec6bca82fd33.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3231330f97a9ed63e96b3735fe904acb",
+    "revision": "d0c131209f6c96b864072b8ce3af284f",
     "url": "/laspesasottocasa/index.html"
   },
   {
-    "revision": "18760b8790d89d51a7a3",
+    "revision": "b7f87fd7078e68224fef",
     "url": "/laspesasottocasa/static/css/main.34de6062.chunk.css"
   },
   {
-    "revision": "99f4a567ef6721bdcdbb",
-    "url": "/laspesasottocasa/static/js/2.939455a1.chunk.js"
+    "revision": "2d3ae429f326622716aa",
+    "url": "/laspesasottocasa/static/js/2.e1c3f67f.chunk.js"
   },
   {
     "revision": "162ad8aa83c335439be5d2720df8ecf9",
-    "url": "/laspesasottocasa/static/js/2.939455a1.chunk.js.LICENSE.txt"
+    "url": "/laspesasottocasa/static/js/2.e1c3f67f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "18760b8790d89d51a7a3",
-    "url": "/laspesasottocasa/static/js/main.af5323b5.chunk.js"
+    "revision": "b7f87fd7078e68224fef",
+    "url": "/laspesasottocasa/static/js/main.38522245.chunk.js"
   },
   {
     "revision": "34890265502a323e913b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/laspesasottocasa/precache-manifest.919fb583f63dea091d3d833a997a4c04.js"
+  "/laspesasottocasa/precache-manifest.f98c4e0f565ddfba53eaec6bca82fd33.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/laspesasottocasa/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
